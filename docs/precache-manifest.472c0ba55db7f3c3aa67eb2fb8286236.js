a946c0c2786788c838e5bbb20fbf8690a69f6ae2
Andre Dinh <dinh.andre@yahoo.com>
2024-10-14 21:12:02 -0700

    Deploy React app to GitHub Pages

---
+++ docs/precache-manifest.472c0ba55db7f3c3aa67eb2fb8286236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffca1d8da9d0ace0406c1d80a136412",
+    "url": "/drug-memorization-app/index.html"
+  },
+  {
+    "revision": "ad183277d0edea4b0427",
+    "url": "/drug-memorization-app/static/css/main.e3d70776.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/drug-memorization-app/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "ad183277d0edea4b0427",
+    "url": "/drug-memorization-app/static/js/main.09810667.chunk.js"
+  },
+  {
+    "revision": "062da507fd0f94cb35b0",
+    "url": "/drug-memorization-app/static/js/runtime~main.53ce1e71.js"
+  }
+]);
