e20ba47e09ad4332f59aeef83defc37d54112917
Andre Dinh <dinh.andre@yahoo.com>
2024-10-16 20:36:07 -0700

    redoply app

---
+++ build/precache-manifest.4c1e4d17111140f3be774fe4ff47c4c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b42dd9992a4e85cb5289657427110f",
+    "url": "/drug-memorization-app/index.html"
+  },
+  {
+    "revision": "78894625e30e61380e9a",
+    "url": "/drug-memorization-app/static/css/main.cbfc5249.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/drug-memorization-app/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "78894625e30e61380e9a",
+    "url": "/drug-memorization-app/static/js/main.7be0f911.chunk.js"
+  },
+  {
+    "revision": "062da507fd0f94cb35b0",
+    "url": "/drug-memorization-app/static/js/runtime~main.53ce1e71.js"
+  }
+]);
